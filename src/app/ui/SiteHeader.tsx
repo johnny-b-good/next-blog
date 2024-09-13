98b05dcb566bd69c90dfd5b515dc7145562e780a
Anton Melnikov <melnikov.anton@gmail.com>
2024-09-13 20:28:46 +0300

    wip

--- src/app/ui/SiteHeader.tsx
+++ src/app/ui/SiteHeader.tsx
@@ -1,7 +1,19 @@
+import { Link } from "./Link";
+
 export const SiteHeader = () => {
   return (
-    <div className="sticky top-0 border-t-4 border-solid border-orange-500 bg-white py-4 text-2xl font-semibold text-cyan-500 shadow-lg">
-      <div className="mx-auto max-w-5xl px-4">Next Blog</div>
+    <div className="sticky top-0 border-t-4 border-solid border-orange-500 bg-white py-4 text-cyan-500 shadow-lg">
+      <div className="mx-auto flex max-w-5xl items-center px-4">
+        <Link className="text-2xl font-semibold no-underline" href="/">
+          Next Blog
+        </Link>
+
+        <div className="flex-1"></div>
+
+        <Link href="/admin" className="justify-self-end">
+          Admin
+        </Link>
+      </div>
     </div>
   );
 };
