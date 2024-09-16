ceb1db66ed4c695a1a62afdc21fda2b614126b03
Anton Melnikov <melnikov.anton@gmail.com>
2024-09-16 13:54:35 +0300

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
+import { getSettings } from "@/lib/queries";
 import "./globals.css";
 import { SiteHeader, SiteFooter } from "@/app/ui";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { siteName, copyright } = await getSettings();
+
   return (
     <html className="min-h-screen bg-slate-50 text-slate-700">
       <body className="grid min-h-screen grid-rows-[min-content_1fr_min-content] items-stretch">
-        <SiteHeader />
+        <SiteHeader siteName={siteName} />
 
         <main>
           <div className="mx-auto max-w-5xl px-4 py-8">{children}</div>
         </main>
 
-        <SiteFooter />
+        <SiteFooter copyright={copyright} />
       </body>
     </html>
   );
--- src/app/ui/SiteHeader.tsx
+++ src/app/ui/SiteHeader.tsx
@@ -1,14 +1,22 @@
+// Lib
+// -----------------------------------------------------------------------------
+import { FC } from "react";
+
 // App
 // -----------------------------------------------------------------------------
 import { Link } from "@/ui";
 
-export const SiteHeader = () => {
+export type SiteHeaderProps = {
+  siteName: string;
+};
+
+export const SiteHeader: FC<SiteHeaderProps> = ({ siteName }) => {
   return (
     <header className="sticky top-0 border-t-4 border-solid border-orange-500 bg-white py-4 shadow-lg">
       <div className="mx-auto flex max-w-5xl items-center px-8">
         <h1>
           <Link className="text-2xl font-semibold no-underline" href="/">
-            Next Blog
+            {siteName}
           </Link>
         </h1>
 
